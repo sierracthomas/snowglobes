48bd0b96d5b2f2d62926f42e80b410009b6dec2d
Sierra Thomas <sierra.c.thomas@gmail.com>
2023-07-12 14:32:51 -0400

    Divide by GeV value instead of bin size

--- scripts/adjust.py
+++ scripts/adjust.py
@@ -9,35 +9,32 @@ data = np.loadtxt(str(sys.argv[1]),
 
 print(f"Starting {str(sys.argv[1])}")
 
-col_1 = data.T[0] * (10**(-3)) # get to units of GeV
+energy_gev = data.T[0] * (10**(-3)) # get to units of GeV
 
-other_cols = (data.T[1] * 10**(-4)) # match units to get to 10^-38 cm^2
-
-print("cols 1", col_1)
-
-# need to extend cross section to 0.2000 GeV
-
-print(min(col_1), max(col_1))
+xs_uneven = (data.T[1] * 10**(-4)) # match units to get to 10^-38 cm^2
 
 #new_x = np.linspace(min(col_1), max(col_1), 1001)
 
+# make evenly spaced bins from 0 -> 0.1 GeV
 new_x = np.linspace(0, 0.1, 1001) # 0 -> 0.1 GeV
 
+# overwrite first entry so as not to div by zero
+# need to update later to start at 5 MeV
 new_x[0] = 10**(-10)
 
-bin_size = new_x[2] - new_x[1]
+#bin_size = new_x[2] - new_x[1]
 
-print(f"bin size is {bin_size}")
 # divide by corresponding energy to get to units with 1/GeV
-#xs = [i/j for i,j in zip(other_cols, col_1)]
-xs = other_cols / bin_size
+
+#xs = other_cols / bin_size
 
 #xs = other_cols
 
 # interpolate between xs numbers to match number of entries in other xs's
 # also, extend the number by one
 
-interp_xs = np.interp(new_x, col_1, xs)
+interp_xs = np.interp(new_x, energy_gev, xs_uneven)
+xs = [i/j for i,j in zip(interp_xs, new_x)]
 
 #interp_xs = np.append(interp_xs, interp_xs[-1])
 
